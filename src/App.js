fab582a80830e676daaad4a52e0859a99ee028f0
Juan Chavez <chavez.gamez.juan12@gmail.com>
2021-05-20 11:19:39 -0500

    rename component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import NavBar from './componentsReact/navbar/NavBar';
-import Home from './componentsReact/homeComp/Home';
+import Home from './componentsReact/home/Home';
 import Exercise from './componentsReact/exerciseComp/Exercise';
 
 class App extends React.Component {
